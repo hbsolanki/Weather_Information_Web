3567b0972a959be8516e9a2607148b499603c38e
Solanki Hardik <sh369915@gmail.com>
2024-03-31 10:07:18 +0530

    Update

--- script.js
+++ script.js
@@ -1,46 +1,113 @@
 let searchBtn = document.getElementById("search");
-let city = "";
+let city = "Ahmedabad";
+
+let allWeatherName = [
+  {
+    name: "smoke",
+    icon: "./Database/app/cloud.png",
+  },
+
+  {
+    name: "clear sky",
+    icon: "./Database/app/clear.png",
+  },
+
+  {
+    name: "haze",
+    icon: "./Database/app/cloud.png",
+  },
+
+  {
+    name: "overcast clouds",
+    icon: "./Database/app/rain.png",
+  },
+
+  {
+    name: "few clouds",
+    icon: "./Database/app/cloud.png",
+  },
+
+  {
+    name: "broken clouds",
+    icon: "./Database/app/cloud.png",
+  },
+
+  {
+    name: "mist",
+    icon: "./Database/app/mist.png",
+  },
+  {
+    name: "scattered clouds",
+    icon: "./Database/app/cloud.png",
+  },
+];
 
 searchBtn.addEventListener("click", async (evt) => {
   let infoBox = document.getElementById("infoBox");
   infoBox.style.visibility = "hidden";
-
-  let p = document.getElementById("notFound");
   let inputBox = document.getElementById("inputBox");
-  p.innerHTML = "";
 
-  evt.preventDefault();
+  city = inputBox.value;
+  let info = await getWeatherInfo();
+  printingDetails(info);
+
+  inputBox.value = "";
+});
+
+const printingDetails = (info) => {
+  console.log(info);
+  let p = document.getElementById("notFound");
+
+  p.innerHTML = "";
 
   try {
     let cityNameShow = document.getElementById("cityNameShow");
-    cityNameShow.innerHTML = inputBox.value;
+    cityNameShow.innerHTML = info.city;
 
-    city = inputBox.value;
-    let info = await getWeatherInfo();
-    console.log(info);
+    let imgForWeather = document.getElementById("imgForWeather");
+    let flag = false;
+
+    for (let i = 0; i < allWeatherName.length; i++) {
+      if (allWeatherName[i].name == info.weather) {
+        console.log(allWeatherName[i]);
+        imgForWeather.src = allWeatherName[i].icon;
+        flag = true;
+      }
+    }
+
+    if (!flag) {
+      imgForWeather.src = "./Database/app/clear.png";
+    }
 
     let infoBox = document.getElementById("infoBox");
     infoBox.style.visibility = "visible";
 
-    let weather = document.getElementById("weather");
-
     let temp = document.getElementById("temp");
-    temp.innerHTML = "temp : " + info.temp + "&deg;C";
+    temp.innerHTML = " Temperature : " + info.temp + "&deg;C";
+
     let humidity = document.getElementById("humidity");
-    humidity.innerHTML = "humidity : " + info.humidity + "%";
+    humidity.innerHTML = " Humidity : " + info.humidity + "%";
+
     let tempMin = document.getElementById("tempMin");
-    tempMin.innerHTML = "tempMin : " + info.tempMin + "&deg;C";
+    tempMin.innerHTML = " Temperature Low : " + info.tempMin + "&deg;C";
+
     let tempMax = document.getElementById("tempMax");
-    tempMax.innerHTML = "tempMax : " + info.tempMax + "&deg;C";
+    tempMax.innerHTML = " Temperature High : " + info.tempMax + "&deg;C";
+
     let feelslike = document.getElementById("feelslike");
-    feelslike.innerHTML = "FeelLike : " + info.feelsLike + "&deg;C";
+    feelslike.innerHTML = " FeelLike : " + info.feelsLike + "&deg;C";
+
+    let pWthr = document.getElementById("weather");
+    pWthr.innerHTML = info.weather;
+
+    let wind = document.getElementById("wind");
+    wind.innerHTML = " Wind Speed : " + info.wind.speed + "km/h";
   } catch (e) {
     p.innerHTML = "No Such Place Exists!";
     p.style.color = "red";
     console.log(e);
   }
-  inputBox.value = "";
-});
+};
 
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "5d5f4c289c06be9fa14d96f7a771106f";
@@ -61,6 +128,10 @@ let getWeatherInfo = async () => {
       humidity: jsonResponse.main.humidity,
       feelsLike: jsonResponse.main.feels_like,
       weather: jsonResponse.weather[0].description,
+      wind: {
+        speed: jsonResponse.wind.speed,
+        deg: jsonResponse.wind.deg,
+      },
 
       sea_level: jsonResponse.main.sea_level,
     };
@@ -69,3 +140,10 @@ let getWeatherInfo = async () => {
     throw err;
   }
 };
+
+let firstTimePrinting = async () => {
+  let data = await getWeatherInfo();
+  printingDetails(data);
+};
+
+firstTimePrinting();
